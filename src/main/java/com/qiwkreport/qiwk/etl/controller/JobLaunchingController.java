48b85af1e8a51c2bb6db84f87b06026710042b9a
Singh, Abhilash [External] <Abhilash.Singh@externals.adidas-group.com>
2017-08-10 20:02:29 +0530

    Added the code for checking the amount of time took to complete the Job

--- src/main/java/com/qiwkreport/qiwk/etl/controller/JobLaunchingController.java
+++ src/main/java/com/qiwkreport/qiwk/etl/controller/JobLaunchingController.java
@@ -1,5 +1,7 @@
 package com.qiwkreport.qiwk.etl.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobInstanceAlreadyExistsException;
 import org.springframework.batch.core.launch.JobOperator;
@@ -17,12 +19,16 @@ public class JobLaunchingController {
 
 	@Autowired
 	private JobOperator jobOperator;
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(JobLaunchingController.class);
 
 	@RequestMapping(value = "qiwk/etl/{batchJobType}", method = RequestMethod.GET)
 	@ResponseStatus(code = HttpStatus.ACCEPTED)
 	public void launch(@PathVariable String batchJobType)
 			throws NoSuchJobException, JobInstanceAlreadyExistsException, JobParametersInvalidException {
+		LOGGER.info("FR Job Started"+System.currentTimeMillis());
 		jobOperator.start(batchJobType,null);
+		LOGGER.info("FR Job Finished"+System.currentTimeMillis());
 	}
 
 }
